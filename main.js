d02777f927c7b09732516c71a8636b05b0e4ad5a
JacvaSipt <121406496+JacvaSipt@users.noreply.github.com>
2023-02-06 20:57:53 +0000

    JS Added Player Choice + Computer Choice

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const game = () => {
     let pScore = 0;
     let cScore = 0;
 
+    //start game
     const startGame = () => {
         const playBtn = document.querySelector('.intro button');
         const introScrn = document.querySelector('.intro');
@@ -10,12 +11,39 @@ const game = () => {
         playBtn.addEventListener('click', () => {
             introScrn.classList.add('fadeOut');
             matchScrn.classList.add('fadeIn');
-        })
+        });
+    };
+
+    //start match
+    const playMatch = () => {
+        const options = document.querySelectorAll('.options button');
+        const playerHand = document.querySelector('.player-hand');
+        const computerrHand = document.querySelector('.computer-hand');
+
+        //Computer options
+        const computerOptions = ['rock', 'paper', 'scissors'];
+
+        options.forEach(option => {
+            option.addEventListener('click', function() {
+                //Computer choice
+                const computerNumber = Math.floor(Math.random() * 3);
+                const computerChoice = computerOptions[computerNumber];
+                console.log(option.textContent);
+                console.log(computerChoice);
+            })
+        });
+
+        
     }
+
+
+
+
 
 
     // Call inner functions
     startGame()
+    playMatch()
 };
 
 // Game start function
